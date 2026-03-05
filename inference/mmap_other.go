79d81a3d39b2e1fe4cc2aa26e1200f45ec44c858
David Ndungu <ndungu.sink@gmail.com>
2026-03-05 12:14:47 -0800

    feat(inference): add WithMmap option for mmap-based model loading
    Adds WithMmap(bool) to inference.Load. When enabled, uses
    model.LoadZMFMmap to memory-map the ZMF file instead of os.ReadFile.
    The mmap mapping is released on Model.Close(). Only supported on unix.
    
    Phase 25 T25.3 + S25.3.1

---
+++ inference/mmap_other.go
@@ -0,0 +1,20 @@
+//go:build !unix
+
+package inference
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/zerfoo/zerfoo/compute"
+	"github.com/zerfoo/zerfoo/model"
+)
+
+// loadZMFWithMmap is not supported on non-unix platforms.
+func loadZMFWithMmap(
+	_ compute.Engine[float32],
+	_ string,
+	_ []model.BuildOption,
+) (*model.Model[float32], io.Closer, error) {
+	return nil, nil, fmt.Errorf("mmap loading is not supported on this platform")
+}
